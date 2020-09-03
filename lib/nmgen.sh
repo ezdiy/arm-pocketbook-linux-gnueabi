45550a550a619285a81fda3f06a99e7ab107334e
Ez Diy <ezdiy@outlook.com>
2020-09-03 07:51:09 +0200

    Add nmgen

---
+++ lib/nmgen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for n in *.so; do
+	if [ ! -e $n ]; then
+		continue
+	fi
+	if [[ $n = *.nm ]]; then
+		continue
+	fi
+	soname=$(objdump -p $n | grep SONAME | awk {'print $2'})
+	nm -f p -D $n > $soname.nm
+done
